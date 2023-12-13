728154f8699c290a920893214e062d8be665fd17
ShindeAryan <shindearyan12nov@gmail.com>
2023-12-13 20:50:54 +0530

    adding files

---
+++ app/src/main/java/com/example/quix/QuestionAnswer.java
@@ -0,0 +1,26 @@
+package com.example.quix;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "The most common storage device for the personal computer is the: ",
+            "Computer forensics involves all of the following stated activities except: ",
+            "The primary component of storage in the personal computer is the: ",
+            "A set of instructions compiled into a program that performs a particular task is known as:"
+    };
+
+    public static String choices[][] = {
+            {"Hard disk drive" ,"USB thumb drive","Floppy disk" ,"Zip disk"  },
+            {"Extraction of computer data","Interpretation of computer data","Preservation of computer data","Manipulation of computer data"},
+            {"NIC","Hard Disk","Media card","Zip disk"},
+            {"Hardware","CPU","Software","Motherboard"}
+    };
+
+    public static String correctAnswers[] = {
+            "Hard disk drive",
+            "Manipulation of computer data",
+            "Hard Disk",
+            "Software"
+    };
+
+}
